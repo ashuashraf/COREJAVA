e79b7b38b292d46350a6548cb14bd9794231ecc0
ashuashraf <96645110+ashuashraf@users.noreply.github.com>
2022-10-18 14:26:45 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    public static void main(String args[])
+    {
+        Car c = new Car();
+        System.out.println(c);
+    }
+    public String toString()
+    {
+        return "This is my car object";
+    }
+}
---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {   
+        String str = "studytonight";
+		
+	//	charAt() method
+	//	String charAt() function returns the character located at the specified index.
+		
+		char index=str.charAt(11); //t
+        System.out.println(index);
+    }
+}
